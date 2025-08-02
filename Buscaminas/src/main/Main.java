8e2ffb99d790385d961aae15e61130e193bed74e
jerrybenjawong <jwonga@est.ups.edu.ec>
2025-08-01 21:30:58 -0500

    Update Main.java
    Este código define la clase Main, que contiene el método principal (main) de inicio de una aplicación en Java. Dentro del método, se crea una instancia de la clase Controlador, que gestiona la lógica del juego. Luego, se llama al método iniciarJuego() para comenzar la ejecución del juego controlado por esa clase.
    Realizada por Jerry Benjamin Wong Arana

---
+++ Buscaminas/src/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+import controlador.Controlador;
+
+public class Main {
+    public static void main(String[] args) {
+        Controlador juego = new Controlador();
+        juego.iniciarJuego();
+    }
+}
