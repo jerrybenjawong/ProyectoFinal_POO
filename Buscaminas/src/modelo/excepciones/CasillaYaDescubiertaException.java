accf87bfaf28dda7081353b564a6f28ca24ecad5
jaredjkf290 <abadfarid89@gmail.com>
2025-08-01 21:26:48 -0500

    Update CasillaYaDescubiertaException.java
    En esta clase se utiliza para indicar que se intento descubrir una casilla que ya se había descubierto anteriormente en el juego este hereda la exception y a su ves recibe un mensaje descriptivo cuándo se lanza
    Elaborado por Jared Abad

---
+++ Buscaminas/src/modelo/excepciones/CasillaYaDescubiertaException.java
@@ -0,0 +1,7 @@
+package modelo.excepciones;
+
+public class CasillaYaDescubiertaException extends Exception {
+    public CasillaYaDescubiertaException(String mensaje) {
+        super(mensaje);
+    }
+}
